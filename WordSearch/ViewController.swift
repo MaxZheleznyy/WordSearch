14f808c968c0fdbed022e65f8dba8afdcf05332e
Max <maxzheleznyy@gmail.com>
2020-08-08 09:44:41 -0700

    Initial Commit

---
+++ WordSearch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WordSearch
+//
+//  Created by Maxim Zheleznyy on 8/8/20.
+//  Copyright © 2020 Maxim Zheleznyy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
