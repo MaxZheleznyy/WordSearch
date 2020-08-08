3aa976f62261c867bac5f9fcdb898f09d79309ff
Max <maxzheleznyy@gmail.com>
2020-08-08 10:08:51 -0700

    Added initial viewModel file

---
+++ WordSearch/ViewModels/WordSearchViewModel.swift
@@ -0,0 +1,17 @@
+//
+//  WordSearchViewModel.swift
+//  WordSearch
+//
+//  Created by Maxim Zheleznyy on 8/8/20.
+//  Copyright © 2020 Maxim Zheleznyy. All rights reserved.
+//
+
+import Foundation
+
+class WordSearchViewModel {
+    
+    private let baseStringedURL = "https://dictionary.skyeng.ru/api/public/v1"
+    private let imageStringedURLPrefix = "https:"
+    
+    
+}
